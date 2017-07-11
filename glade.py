0cc7cdf1c53d5e44a163bd43ac0733bef91019b7
robertohueso <robertohueso96@gmail.com>
2017-07-11 18:15:26 +0200

    Add gui tabs

--- glade.py
+++ glade.py
@@ -5,3 +5,6 @@ GLADE_DIR = os.path.join(os.path.dirname(__file__), './glade/')
 get = functools.partial(os.path.join, GLADE_DIR)
 
 GUI_MAIN = get('main.glade')
+GUI_OPEN = get('open.glade')
+GUI_CLOSE = get('close.glade')
+GUI_CREATE = get('create.glade')
--- gui.py
+++ gui.py
@@ -10,9 +10,27 @@ handlers = {
 
 builder = Gtk.Builder()
 builder.add_from_file(glade.GUI_MAIN)
+builder.add_from_file(glade.GUI_OPEN)
+builder.add_from_file(glade.GUI_CLOSE)
+builder.add_from_file(glade.GUI_CREATE)
 builder.connect_signals(handlers)
 
 main_window = builder.get_object('main_window')
+notebook = builder.get_object('notebook')
+
+#Tabs labels
+open_label = Gtk.Label('Open')
+close_label = Gtk.Label('Close')
+create_label = Gtk.Label('Create')
+
+#Add tabs
+gui_open = builder.get_object('gui_open')
+gui_close = builder.get_object('gui_close')
+gui_create = builder.get_object('gui_create')
+notebook.append_page(gui_open, open_label)
+notebook.append_page(gui_close, close_label)
+notebook.append_page(gui_create, create_label)
+
 main_window.show_all()
 
 Gtk.main()
