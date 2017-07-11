8dbefa881c4cde82e2eb2d60e22a6701411d7582
robertohueso <robertohueso96@gmail.com>
2017-07-12 01:58:48 +0200

    Improve gui structure

--- gui.py
+++ gui.py
@@ -4,33 +4,46 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import glade
 
-handlers = {
-    'onDeleteWindow': Gtk.main_quit
-}
+class MainWindow():
+    
+    def __init__(self):
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file(glade.GUI_MAIN)
+        self.open_box = OpenBox(self)
+        self.builder.add_from_file(glade.GUI_CLOSE)
+        self.builder.add_from_file(glade.GUI_CREATE)
+        #Tabs labels
+        self.open_label = Gtk.Label('Open')
+        self.close_label = Gtk.Label('Close')
+        self.create_label = Gtk.Label('Create')
+        
+        self.builder.connect_signals(self)
+        
+        self.main_window = self.builder.get_object('main_window')
+        self.notebook = self.builder.get_object('notebook')
+        self.gui_open = self.builder.get_object('gui_open')
+        self.gui_close = self.builder.get_object('gui_close')
+        self.gui_create = self.builder.get_object('gui_create')
+        
+        self.notebook.append_page(self.gui_open, self.open_label)
+        self.notebook.append_page(self.gui_close, self.close_label)
+        self.notebook.append_page(self.gui_create, self.create_label)
+        
+    def onDeleteWindow(self, *args):
+        return Gtk.main_quit(*args)
+    
+    def run(self):
+        self.main_window.show_all()
+        Gtk.main()
 
-builder = Gtk.Builder()
-builder.add_from_file(glade.GUI_MAIN)
-builder.add_from_file(glade.GUI_OPEN)
-builder.add_from_file(glade.GUI_CLOSE)
-builder.add_from_file(glade.GUI_CREATE)
-builder.connect_signals(handlers)
+class OpenBox():
 
-main_window = builder.get_object('main_window')
-notebook = builder.get_object('notebook')
+    def __init__(self, parent):
+        self.builder = parent.builder
+        self.builder.add_from_file(glade.GUI_OPEN)
+        #Connect signals
+        args = 'clicked', self.handle_open_device
+        self.builder.get_object('open-device-button').connect(*args)
 
-#Tabs labels
-open_label = Gtk.Label('Open')
-close_label = Gtk.Label('Close')
-create_label = Gtk.Label('Create')
-
-#Add tabs
-gui_open = builder.get_object('gui_open')
-gui_close = builder.get_object('gui_close')
-gui_create = builder.get_object('gui_create')
-notebook.append_page(gui_open, open_label)
-notebook.append_page(gui_close, close_label)
-notebook.append_page(gui_create, create_label)
-
-main_window.show_all()
-
-Gtk.main()
+    def handle_open_device(self, widget):
+        print("Placeholder")
---
+++ lukr-gui.py
@@ -0,0 +1,5 @@
+import gui
+
+if __name__ == '__main__':
+    lukr_gui = gui.MainWindow()
+    lukr_gui.run()
